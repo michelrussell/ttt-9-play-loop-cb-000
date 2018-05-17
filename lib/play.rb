318262f92fbae7d06ea35b9be98be5699ea8c09c
Michel Russell <michelrussell2014@gmail.com>
2018-05-17 15:26:33 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,9 +38,10 @@ end
 # Define your play method below
 def play(board)
   total_turns = 0
-  while total_turns = 0
-  turn(board)
-  total_turns +=
+    while total_turns = 0
+    turn(board)
+    total_turns +=
+  end
 end
 
 
